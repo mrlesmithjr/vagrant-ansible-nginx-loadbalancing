e1cdb84e389dab74a71cf459740851ca0e50f9a1
Larry Smith Jr <mrlesmithjr@gmail.com>
2015-09-15 19:07:39 -0400

    fixed incorrect load-balancer hosts

--- update_hosts.sh
+++ update_hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "" >> hosts
 echo "[load-balancers]" >> hosts
-echo "mysql-[1:2]" >> hosts
+echo "lb-[1:2]" >> hosts
 echo "" >> hosts
 echo "[mysql-nodes]" >> hosts
 echo "mysql-[1:2]" >> hosts
